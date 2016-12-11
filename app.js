02d859a8d8325bbbf248c6158b8685c41367ab1c
shihabmridha <shihabmridha@gmail.com>
2016-12-11 22:59:25 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+/**
+ * Created by Shihab Mridha
+ * on 05-Dec-16.
+ */
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    request = require('request');
+
+var app = express();
+
+
+/**
+ * View Engine
+ */
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+/**
+ * Body-parser Middleware
+ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+/**
+ * Set static path
+ */
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', function (req, res) {
+    res.render('index',{
+        status : 200,
+        display: 'none',
+        src: ''
+    });
+});
+
+app.post('/', function (req, res) {
+    var username = req.body.username;
+    request('https://www.instagram.com/' + username, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var indexOf = body.indexOf("profile_pic_url");
+            var url = '';
+            for(var i = indexOf+22; i <= body.length; i++){
+                if(body[i+1] != ',' ){
+                    url += body[i];
+                }else{
+                    break;
+                }
+            }
+
+            var temp = url.split("/");
+            url = temp[0] + "//" + temp[2] + "/" + temp[3] + "/" + temp[5];
+
+            res.render('index',{
+                status : 200,
+                display: 'block',
+                src: url
+            });
+
+        }else{
+            res.render('index',{
+                status : response.statusCode,
+            });
+        }
+    });
+});
+
+/**
+ * Setup listener port
+ */
+app.listen(3000,function () {
+    console.log("App is listening to port 3000 !");
+});
